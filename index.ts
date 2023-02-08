c5cf88846b75ba4f4ee27f3949026586a3e4aafc
Dominique Chin-Jones <domecj13@gmail.com>
2023-02-08 17:53:10 -0500

    create main function with prisma query

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-// TODO
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: "Dominique",
+            email: "test@email.com",
+        },
+    });
+    console.log(user);
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });

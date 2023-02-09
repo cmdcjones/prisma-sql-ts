fa413ba0170b0da3557c96b1f7981708f4a67aed
Dominique Chin-Jones <domecj13@gmail.com>
2023-02-08 19:45:43 -0500

    add records to dev.db through prisma interface

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+/* 
+    create a record in the User table
     const user = await prisma.user.create({
         data: {
             name: "Dominique",
@@ -10,6 +12,34 @@ async function main() {
         },
     });
     console.log(user);
+----------------------------------------
+    use findMany method to retrieve all records in User table
+    const users = await prisma.user.findMany();
+    console.log(users)
+----------------------------------------
+    create a relational record in the User and Posts table
+    const user = await prisma.user.create({
+        data: {
+            name: "User with Post",
+            email: "post@email.com",
+            posts: {
+                create: {
+                    title: "Hello, world!",
+                },
+            },
+        },
+    });
+    console.log(user);
+----------------------------------------
+    use findMany method to retrieve all records in User table; this time including related Posts records
+    const usersWithPosts = await prisma.user.findMany({
+        include: {
+            posts: true,
+        },
+    });
+    console.dir(usersWithPosts, { depth: null });
+----------------------------------------
+*/
 }
 
 main()
